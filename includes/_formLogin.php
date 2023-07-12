5494b0ad16da2db6cd2f0e499cce30fb47978c5a
VictorPeller1 <victorpellerin14@gmail.com>
2023-07-12 11:06:37 +0200

    fix responsive & container for the last commit

--- includes/_formLogin.php
+++ includes/_formLogin.php
@@ -1,11 +1,12 @@
 <form class="form" action="login.php" method="POST">
 <img class="form__logo" src="assets/logo/yokami_logo_white.png" alt="">
-
+    <div class="form__container">
     <label class="form__label" for="">Email:</label>
     <input type="text" name="someone_email">
     <br>
     <label class="form__label" for="">Mot de passe:</label>
     <input type="password" name="someone_pwd">
+    </div>
     <br>
     <input class="form__btn" type="submit" name="login" value="Se connecter">
     <a class="form__lnk" href="register.php">Create an account </a>
