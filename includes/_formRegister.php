da084d5ed27b4c0cc17641243d73432d1779866e
VictorPeller1 <victorpellerin14@gmail.com>
2023-07-12 11:20:27 +0200

    forgot the container on the formRegister.php

--- includes/_formRegister.php
+++ includes/_formRegister.php
@@ -1,5 +1,6 @@
 <form class="form" action="register.php" method="post">
     <img class="form__logo" src="assets/logo/yokami_logo_white.png" alt="">
+    <div class="form__container">
     <label class="form__label" for="">Nom :</label>
     <input type="text" name="someone_name">
     <br>
@@ -11,5 +12,6 @@
     <br>
     <input class="form__btn" type="submit" name="valider" value="Valider">
     <br>
+    </div>
     <a class="form__lnk" href="login.php">Already an account ?</a>
 </form>
