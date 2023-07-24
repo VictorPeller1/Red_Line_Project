a812f48b4a2c763d5cda9f79ac0034de3813dcae
VictorPeller1 <victorpellerin14@gmail.com>
2023-07-24 21:20:00 +0200

    add transition on JS hover card

--- script.js
+++ script.js
@@ -91,12 +91,12 @@ console.log(cards);
 cards.forEach(card => {
   card.addEventListener('mouseover', () => {
     card.style.transform = "scale(1.4)";
-    card.style.position="relative";
-    //card.style.z-index="1";
+    card.style.transition = "400ms";
   });
 
   card.addEventListener('mouseout', () => {
     card.style.transform = "scale(1)";
+    card.style.transition = "400ms";
   });
 });
 
