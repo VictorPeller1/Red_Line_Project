bf6efaebe116056fdf6f44ef6e7a13d6eaee8b6f
VictorPeller1 <victorpellerin14@gmail.com>
2023-07-30 02:00:21 +0200

    merge each card in a foreach

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-// test
 require_once './vendor/autoload.php';
 require './includes/_database.php';
 include 'includes/_head.php';
@@ -14,6 +13,7 @@ $query->execute();
 $resultsYokai = $query->fetchAll();
 // var_dump($resultsYokai)
 ?>
+
 <?php
 $query = $dbCo->prepare("SELECT article_title, article_img FROM article WHERE id_category=1 LIMIT 2");
 $query->execute();
@@ -41,15 +41,14 @@ $resultsKami = $query->fetchAll();
       <h2 class="article__ttl">Yōkais</h2>
       <p class="article__txt"> Les yōkai (妖怪, « esprit », « fantôme », « démon », « apparition étrange ») sont un type de créatures surnaturelles dans le folklore japonais. Ils sont souvent représentés comme des esprits malfaisants ou simplement malicieux démontrant les tracas quotidiens ou inhabituels. </p>
       <div class="card-container">
-        <a href="article.php" class="card">
-          <img class="card__img" src="<?= $resultsYokai[0]['article_img'] ?>" alt="">
-          <li class="card__ttl"><?= $resultsYokai[0]['article_title'] ?></li>
-        </a>
 
-        <a href="article.php" class="card">
-          <img class="card__img" src=" <?= $resultsYokai[1]['article_img'] ?>" alt="">
-          <p class="card__ttl"><?= $resultsYokai[1]['article_title'] ?></p>
-        </a>
+        <?php foreach ($resultsYokai as $result): ?>
+          <a href="article.php" class="card">
+          <img class="card__img" src="<?= $result['article_img'] ?>" alt="">
+          <li class="card__ttl"><?= $result['article_title'] ?></li>
+          </a>
+          <?php endforeach; ?>
+      </div>
     </article>
 
     <article class="article">
@@ -57,56 +56,54 @@ $resultsKami = $query->fetchAll();
       <p class="article__txt"> Un kami (神) est une divinité ou un esprit vénéré dans la religion shintoïste. <br>Leur équivalent chinois est shen.<br> Les kamis sont la plupart du temps des éléments de la nature, des animaux ou des forces créatrices de l'univers, mais peuvent aussi être des esprits de personnes décédées.<br> </p>
       <div class="card-container">
 
-        <a href="article.php" class="card">
-          <img class="card__img" src="<?= $resultsKami[0]['article_img'] ?>" alt="">
-          <p class="card__ttl"><?= $resultsKami[0]['article_title'] ?></p>
-        </a>
-        <a href="article.php" class="card">
-          <img class="card__img" src=" <?= $resultsKami[1]['article_img'] ?>" alt="">
-          <p class="card__ttl"><?= $resultsKami[1]['article_title'] ?></p>
-        </a>
+<?php foreach ($resultsKami as $result): ?>
+  <a href="article.php" class="card">
+    <img class="card__img" src="<?= $result['article_img'] ?>" alt="">
+    <p class="card__ttl"><?= $result['article_title'] ?></p>
+  </a>
+<?php endforeach; ?>
+      </div>
     </article>
 
   </section>
 
-  <?php
-  //    $file = 'article.json'; // Chemin vers le fichier JSON
-  //
-  //    // Lecture du contenu du fichier JSON
-  //    $jsonData = file_get_contents($file);
-  //
-  //    // Vérification de la validité du JSON
-  //    if ($jsonData === false) {
-  //      echo "Erreur lors de la lecture du fichier JSON.";
-  //      exit;
-  //    }
-  //
-  //    // Conversion du JSON en tableau associatif
-  //    $data = json_decode($jsonData, true);
-  //
-  //    if ($data !== null) {
-  //      if (isset($data['article'])) {
-  //        $articles = $data['article'];
-  //
-  //        foreach ($articles as $article) {
-  //          echo $article['Id_article'] . "<br>";
-  //          echo $article['article_title'] . "<br>";
-  //          echo $article['article_content'] . "<br>";
-  //          echo $article['article_date'] . "<br>";
-  //          echo $article['Id_Validation'] . "<br>";
-  //          echo $article['Id_category'] . "<br>";
-  //          echo $article['Id_someone'] . "<br>";
-  //          echo "<br>";
-  //        }
-  //      } else {
-  //        echo "Aucun article trouvé.";
-  //      }
-  //    } else {
-  //      echo "JSON invalide.";
-  //    }
-  //
-  //    
-  ?>
+<?php
+//    $file = 'article.json'; // Chemin vers le fichier JSON
+//
+//    // Lecture du contenu du fichier JSON
+//    $jsonData = file_get_contents($file);
+//
+//    // Vérification de la validité du JSON
+//    if ($jsonData === false) {
+//      echo "Erreur lors de la lecture du fichier JSON.";
+//      exit;
+//    }
+//
+//    // Conversion du JSON en tableau associatif
+//    $data = json_decode($jsonData, true);
+//
+//    if ($data !== null) {
+//      if (isset($data['article'])) {
+//        $articles = $data['article'];
+//
+//        foreach ($articles as $article) {
+//          echo $article['Id_article'] . "<br>";
+//          echo $article['article_title'] . "<br>";
+//          echo $article['article_content'] . "<br>";
+//          echo $article['article_date'] . "<br>";
+//          echo $article['Id_Validation'] . "<br>";
+//          echo $article['Id_category'] . "<br>";
+//          echo $article['Id_someone'] . "<br>";
+//          echo "<br>";
+//        }
+//      } else {
+//        echo "Aucun article trouvé.";
+//      }
+//    } else {
+//      echo "JSON invalide.";
+//    }
+//
+?>
 
   <?php include 'includes/_footer.php' ?>
 
--- script.js
+++ script.js
@@ -89,15 +89,15 @@ const cards = document.querySelectorAll(".card");
 console.log(cards);
 
 cards.forEach(card => {
-  card.addEventListener('mouseover', () => {
-    card.style.transform = "scale(1.4)";
-    card.style.position="relative";
-    //card.style.z-index="1";
-  });
+    card.addEventListener('mouseover', () => {
+        card.style.transform = "scale(1.4)";
+        card.style.position = "relative";
+        //card.style.z-index="1";
+    });
 
-  card.addEventListener('mouseout', () => {
-    card.style.transform = "scale(1)";
-  });
+    card.addEventListener('mouseout', () => {
+        card.style.transform = "scale(1)";
+    });
 });
 
 // ------------ HIDDEN HEADER ON SCROLL ----------------
@@ -108,20 +108,9 @@ console.log(header);
 
 window.addEventListener('scroll', () => {
 
-  if (window.scrollY < 400) {
-    header.style.top = "0";
-  } else {
-    header.style.top = "-100px";
-  }
+    if (window.scrollY < 400) {
+        header.style.top = "0";
+    } else {
+        header.style.top = "-100px";
+    }
 })
-
-
-
-
-
-
-
-
-
-
-
