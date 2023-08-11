ff62fd7e550e1832cba708e8b4d134b2fe4aa37b
VictorPeller1 <victorpellerin14@gmail.com>
2023-08-11 15:59:53 +0200

    user pwd hash on DB & security update

--- register.php
+++ register.php
@@ -1,3 +1,4 @@
+
 <?php
 require_once './vendor/autoload.php';
 require 'includes/_database.php';
@@ -5,33 +6,39 @@ include 'includes/_head.php';
 
 session_start();
 
-//-----------------------------------------------------------------------------------------------------------------------------
-
 if (isset($_POST['valider'])) {
     $someone_name = $_POST['someone_name'];
     $someone_email = $_POST['someone_email'];
     $someone_pwd = $_POST['someone_pwd'];
 
+    // Hasher le mot de passe
+    $hashed_password = password_hash($someone_pwd, PASSWORD_DEFAULT);
+
     $mySQL = "INSERT INTO `someone` (`someone_name`, `someone_email`, `someone_pwd`)
-     VALUES (:someone_name, :someone_email, :someone_pwd)";
+              VALUES (:someone_name, :someone_email, :someone_pwd)";
+    
+    // Préparation de la requête
     $query = $dbCo->prepare($mySQL);
     $query->bindParam(':someone_name', $someone_name);
     $query->bindParam(':someone_email', $someone_email);
-    $query->bindParam(':someone_pwd', $someone_pwd);
+    $query->bindParam(':someone_pwd', $hashed_password);
+
+    // Exécution de la requête
     $response = $query->execute();
+
     if ($response) {
         echo "L'inscription est réussie";
-        header("Location: contribute.php");
+        header("Location: contribute.php"); // Rediriger vers la page de connexion
+        exit();
     } else {
-        echo "Erreur lors de l'inscription";
+    echo "Erreur lors de l'inscription";
+        header("Location: register.php"); // Rediriger vers la page de connexion
     }
-};
-
+}
 ?>
-
-
 
 <?php include 'includes/_header.php' ?>
 
 <?php include 'includes/_formRegister.php' ?>
+
 
